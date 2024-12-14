a650295788ee6a05567bef9d8ff3fd3bec167fe0
Ari <79033264+KotaNofa@users.noreply.github.com>
2024-12-13 16:48:43 -0800

    Added matrix constructor init to zero

--- Engine.cpp
+++ Engine.cpp
@@ -6,7 +6,7 @@ using namespace sf;
 
 Engine::Engine()
 {
-	m_Window.create(VideoMode(600, 600), "Particles!! by Armin and Daniel");
+	m_Window.create(VideoMode(desktop.width / 2, desktop.height / 2), "Particles!! by Armin and Daniel");
 }
 
 void Engine::run()
--- Matrices.cpp
+++ Matrices.cpp
@@ -1,1 +1,19 @@
 #include "Matrices.h"
+
+namespace Matrices
+{
+	Matrix::Matrix(int _rows, int _cols) {
+		rows = _rows;
+		cols = _cols;
+		a.resize(rows);	
+		for (int i = 0; i < rows; ++i) {
+			a[i].resize(cols);
+		}
+		for (int r = 0; r < rows; ++r) {
+			for (int c = 0; c < cols; ++c) {
+				a[r][c] = 0;
+			}
+		}
+	}
+
+}
